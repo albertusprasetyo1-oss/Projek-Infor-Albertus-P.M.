1d18a7cb3ab72152a462040e370a3f7d76cedda1
Albertus <albertusprasetyo1@gmail.com>
2026-05-21 01:36:33 +0700

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
-import HomeClient from '@/components/home-client'
-
 export default function HomePage() {
   return (
-    <>
-      <Navbar />
-      <HomeClient />
-      <Footer />
-    </>
+    <div>
+      <h1>WEBSITE BERHASIL ONLINE 🚀</h1>
+    </div>
   )
 }
